3e4c81061d4f2c3fe659a83f79e7680a6c8c5312
Tal <tal.bny3@gmail.com>
2020-02-14 16:11:09 +0200

    edited a bit the stories, and added another one

--- app/src/main/java/com/taban/generaterealitystory/StoryCollection.kt
+++ app/src/main/java/com/taban/generaterealitystory/StoryCollection.kt
@@ -5,10 +5,11 @@ import kotlin.collections.ArrayList
 
 class StoryCollection {
 
-    var stories = ArrayList(Arrays.asList("I have two fathers. I have been bullied since I was 8 years old, for 4 years! At the age 12, I decided to change school, and never told about my parents. I have always hidden it, and every time someone asked about them I used to change the subject. Only recently I ",
-        "My mom died when I was 4 years old. I grew up with my dad and my twin sister. My father was barely home. My mom death was hard for him, so he kept himself busy at work. So it was like to grow up with out my dad too. My sister and I grew up alone. We helped each other, and raised each other. It wasn't easy for us to grow with out our parents.",
-        "I took a surgery to change sex. All my life I felt I lived in the wrong body.",
-        "I'm alcoholic. I can't start a day without  several drinks. It started when my girlfriend left me a year ago, and I took it very hard. I started drinking every day, and than every morning. It is hard the make the day throw, butI try to work on it. I'm at a support group, which we help each other, and it helps. I have a message for the audience, never give up."
+    var stories = ArrayList(Arrays.asList("I have two fathers. I have been bullied since I was 8 years old, for 4 years! At the age 12, I decided to switch school, and never told about my parents. I have always hidden it, and every time someone asked about them I changed the subject.",
+        "My mom died when I was 4 years old. I grew up with my dad and my twin sister. My father was barely home. My mom's death was hard for him, so he kept himself busy at work. So it was like I grew up with out my dad too. My sister and I grew up alone. We helped each other, and raised each other. It wasn't easy for us to grow with out our parents.",
+        "I took a surgery to change sex. All my life I felt that I lived in the wrong body.",
+        "I'm alcoholic. I can't start a day without  several drinks. It started when my girlfriend left me a year ago, and I took it very hard. I started drinking once a while, and than every morning. It is hard the make the day throw, but I try to work on it. I'm at a support group, which we help each other. I have a message for the audience, never give up.",
+        "My wife and I cant make a baby. We tried for more than a year but it never worked. After we made tests, it turned out that we cant, and never will. So we decided to adopt a 7 years old kid, who could not find a loving home. He is the kindest. We gave him home, and love, and a normal happy family. Looking back, Im happy we could not get a baby, and gave a good life for a lonely kid."
         ))
 
     init {
