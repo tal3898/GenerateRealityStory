4e03112a98c5b6cd2bc13b3eaa73fd27f9fae511
Tal <tal.bny3@gmail.com>
2020-03-08 00:04:56 +0200

    made title layout

--- app/src/main/java/com/taban/generaterealitystory/MainActivity.kt
+++ app/src/main/java/com/taban/generaterealitystory/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ModelPreferencesManager.with(this)
 
-        storyTxt.setMovementMethod(ScrollingMovementMethod())
 
         MobileAds.initialize(this) {}
 
@@ -39,54 +38,5 @@ class MainActivity : AppCompatActivity() {
         val externalStoriesLoader = ExternalStoriesLoader(stories)
         externalStoriesLoader.load()
 
-
-        // fullad
-        mInterstitialAd = InterstitialAd(this)
-        mInterstitialAd.adUnitId = "ca-app-pub-5267648725343207/7092529294"
-        // mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712" //test id
-
-        // banner ad
-        val mAdView = adView
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
-
-        mAdView.adListener = object: AdListener() {
-            override fun onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-            }
-
-            override fun onAdFailedToLoad(errorCode : Int) {
-                // Code to be executed when an ad request fails.
-            }
-
-            override fun onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-
-            override fun onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            override fun onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-            }
-
-            override fun onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
-            }
-        }
-    }
-
-    fun showRandomStory(view: View) {
-        var storyTextView:TextView = storyTxt
-        var newStory:String = stories.getStory()
-        storyTextView.setText(newStory)
-
-        if ((isShowingAdClick++)% showingAdClicksInterval == 0) {
-            mInterstitialAd.loadAd(AdRequest.Builder().build())
-            mInterstitialAd.show()
-        }
     }
 }
